1c334a1f0167424651e9e0118fc2d32a5c146c78
PataiSzabolcs <pataiszabolcs@gmail.com>
2017-12-14 11:06:54 +0100

    feat(application skeleton):creating program, filehandler and printer class

---
+++ TODO/TODO/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TODO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RunApplication(args);
+        }
+        // remove static?
+        public static void RunApplication(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Command Line Todo application");
+                Console.WriteLine("=============================");
+                Console.WriteLine();
+                Console.WriteLine("Command line arguments:");
+                Console.WriteLine(" -l\tLists all the tasks");
+                Console.WriteLine(" -a\tAdds a new task");
+                Console.WriteLine(" -r\tRemoves an task");
+                Console.WriteLine(" -c\tCompletes an task");
+            }
+        }
+    }
+}
